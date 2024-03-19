2d0dab4ad4c29b357ca9a56c99fc81d29b2add2b
Oscar Ivan <oscar.moreno4275@alumnos.udg.mx>
2024-03-19 19:37:26 +0100

    dynamic interface added

---
+++ js/graphEditor.js
@@ -0,0 +1,81 @@
+class GraphEditor {
+    constructor(canvas, graph){
+        this.canvas = canvas;
+        this.graph = graph;
+
+        this.context = this.canvas.getContext("2d");
+        this.selected = null;
+        this.hovered = null;
+        this.dragging = false;
+        this.mouse = null;
+
+        this.#addEventListeners();
+    }
+    #addEventListeners(){
+        this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+        //tio remove left click menu
+        this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
+        // this makes the dragg thing to return to false
+        this.canvas.addEventListener("mouseup",()=> this.dragging= false);
+                
+    }
+
+    #select(point){
+        if (this.selected){
+            this.graph.tryAddSegment(new Segment(this.selected,point));
+        }
+        this.selected = point;
+    }
+
+    #handleMouseDown(evt){
+        if (evt.button == 2){ //right click
+            if(this.selected){
+                this.selected = null;
+            } else if (this.hovered) {
+                this.#removePoint(this.hovered);
+            }
+        }
+        if (evt.button == 0){ //left click
+            if (this.hovered){
+                this.#select(this.hovered);
+                this.dragging = true;
+                return;
+            }
+            // we add the new point
+            this.graph.addPoint(this.mouse);
+            this.#select(this.mouse);
+            this.hovered = this.mouse;
+            
+        }
+    }
+    #handleMouseMove(evt){
+        this.mouse = new Point(evt.offsetX, evt.offsetY);
+        this.hovered = getNearestPoint(this.mouse, this.graph.points, 12);
+        if (this.dragging == true){
+            this.selected.x = this.mouse.x;
+            this.selected.y = this.mouse.y;
+        }
+    }
+    //this remove the selection when ever we remove the point;
+    #removePoint(point){
+        this.graph.removePoint(point);
+        this.hovered = null;
+        if (this.selected == point){
+            this.selected = null;
+        }
+    }
+
+    display(){
+        this.graph.draw(this.context);
+        if (this.hovered) {
+            this.hovered.draw(this.context, {fill: true});
+        }
+        if (this.selected) {
+            // here we make the intended segmet stick to the hovered point
+            const intent = this.hovered ? this.hovered : this.mouse;
+            new Segment(this.selected, this.mouse).draw(context, {dash: [3,3]});
+            this.selected.draw(this.context, {outline: true});
+        }
+    }
+}
--- js/primitives/point.js
+++ js/primitives/point.js
@@ -8,11 +8,24 @@ class Point{
         return this.x == point.x && this.y == point.y;
     }
 
-    draw(context , size = 18, color = "black"){
+    draw(context,{size = 18, color = "black",outline = false, fill = false} = {}){
         const rad = size / 2;
         context.beginPath();
         context.fillStyle = color;
         context.arc(this.x, this.y, rad, 0, Math.PI * 2);
         context.fill();
+        if (outline){
+            context.beginPath();
+            context.lineWidth = 3;
+            context.strokeStyle = "white";
+            context.arc(this.x, this.y, rad*0.6, 0, Math.PI * 2);
+            context.stroke();
+        }
+        if (fill){
+            context.beginPath();
+            context.arc(this.x, this.y, rad * 0.4, 0, Math.PI*2);
+            context.fillStyle = "violet";
+            context.fill();
+        }
     }
 }
